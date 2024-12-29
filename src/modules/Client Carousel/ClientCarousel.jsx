7ed6c13ff330589d8f3f2b7d9b1d137a2f21486b
fahim-khandakar <fahimkhandakar01@gmail.com>
2024-12-29 12:16:33 +0600

    client carousel ok

--- src/modules/Client Carousel/ClientCarousel.jsx
+++ src/modules/Client Carousel/ClientCarousel.jsx
@@ -1,5 +1,56 @@
-const ClientCarousel = () => {
-  return <div>Client Carousel</div>;
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+import "./styles.css";
+import { carouselData } from "./config/constant";
+
+const carousel = (slider) => {
+  const z = 600;
+  function rotate() {
+    const deg = 360 * slider.track.details.progress;
+    slider.container.style.transform = `translateZ(-${z}px) rotateY(${-deg}deg)`;
+  }
+  slider.on("created", () => {
+    const deg = 360 / slider.slides.length;
+    slider.slides.forEach((element, idx) => {
+      element.style.transform = `rotateY(${deg * idx}deg) translateZ(${z}px)`;
+    });
+    rotate();
+  });
+  slider.on("detailsChanged", rotate);
 };
 
-export default ClientCarousel;
+export default function ClientCarousel() {
+  const [sliderRef] = useKeenSlider(
+    {
+      loop: true,
+      selector: ".carousel__cell",
+      renderMode: "custom",
+      mode: "free-snap",
+    },
+    [carousel]
+  );
+
+  return (
+    <section className="py-20">
+      <div className="wrapper">
+        <div className="scene">
+          <div className="carousel keen-slider" ref={sliderRef}>
+            {carouselData?.carousels?.map((item, index) => (
+              <div key={index} className="carousel__cell  ">
+                <img
+                  className="h-full w-full rounded-xl"
+                  src={item?.img}
+                  alt="Carousel Image"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="mt-72 px-20 space-y-5 text-formTitle">
+        <p>{carouselData?.descriptionOne}</p>
+        <p>{carouselData?.descriptionOne}</p>
+      </div>
+    </section>
+  );
+}
---
+++ src/modules/Client Carousel/config/constant.js
@@ -0,0 +1,33 @@
+import bgImage from "../../../assets/chino-rocha-v_sciNJhbnM-unsplash.jpg";
+import bgImage2 from "../../../assets/ilya-mirnyy-B95NRMrXN6M-unsplash.jpg";
+import bgImage3 from "../../../assets/pedram-normohamadian-2TLRbbBlY94-unsplash.jpg";
+
+export const carouselData = {
+  carousels: [
+    {
+      img: bgImage,
+    },
+    {
+      img: bgImage2,
+    },
+    {
+      img: bgImage,
+    },
+    {
+      img: bgImage3,
+    },
+    {
+      img: bgImage,
+    },
+    {
+      img: bgImage3,
+    },
+    {
+      img: bgImage2,
+    },
+  ],
+  descriptionOne:
+    "Bij GMK Dakonderhoud combineren we vakmanschap met passie voor daken. Onze specialisten zijn deskundig in het aanbrengen en onderhouden van diverse soorten dakbedekkingen, waaronder dakpannen, bitumen en EPDM. Of het nu gaat om dakisolatie, dakreiniging of het repareren van lekkages, wij behandelen elke klus met de hoogste zorg en aandacht.",
+  descriptionTwo:
+    "Onze focus ligt op kwaliteit en duurzaamheid, zodat uw dak niet alleen functioneel is, maar ook esthetisch aantrekkelijk. Klanten waarderen onze snelle service en professionele oplossingen. Bekijk onze projecten en ontdek hoe wij uw dak kunnen transformeren met de juiste materialen en technieken voor een langdurige bescherming.",
+};
