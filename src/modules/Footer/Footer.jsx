c4313f581a1110aea167ff227143376962080ad7
fahim-khandakar <fahimkhandakar01@gmail.com>
2024-12-29 14:31:28 +0600

    project done

--- src/modules/Footer/Footer.jsx
+++ src/modules/Footer/Footer.jsx
@@ -1,5 +1,23 @@
+import { data } from "./config/constant";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="flex flex-col md:flex-row justify-center md:justify-between items-center md:items-end  px-5">
+        <h3 className="text-[50px] md:text-[96px]">{data?.leftTitle}</h3>
+        <h1 className="text-[150px] md:text-[347px] font-bold">
+          {data?.rightTitle}
+        </h1>
+      </div>
+      <hr />
+      <div className="grid grid-cols-2 gap-5 md:grid-cols-4  text-lg p-5">
+        <p>{data?.bottomOne}</p>
+        <p>{data?.bottomTwo}</p>
+        <p>{data?.bottomThree}</p>
+        <p>{data?.bottomFour}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
---
+++ src/modules/Footer/config/constant.js
@@ -0,0 +1,13 @@
+export const data = {
+  leftTitle: `Overtuigd van de nummer 1
+in Noord Holland ?`,
+  rightTitle: `GMK`,
+  bottomOne: `GMK Dakonderhoud B.V.
+Leidsekade 57 1016 CX Amsterdam`,
+  bottomTwo: `info@dakonderhoudgmk.nl
+(020) 26 12 257`,
+  bottomThree: `Privacy verklaring
+Algemene Voorwaarden`,
+  bottomFour: `(C) MMXXIV GMK Dakonderhoud B.V.
+Concept by Nakatori Amsterdam`,
+};
